e7221b681a5a4b4f0a7f5ef2fe7dab103af03af9
mshich <mollie.shichman@gmail.com>
2024-09-10 14:40:11 -0400

    finished script for getting gemini baseline(s)

--- gemini/get_gemini_jsonl.py
+++ gemini/get_gemini_jsonl.py
@@ -4,15 +4,31 @@
 import google.generativeai as genai
 import os
 import json
+from ratelimit import limits
 
-file_name = input("put in the relative directory and jsonl file name that you want to send: ")
+MINUTE = 60
 
-instruct = [json.loads(l) for l in open(file_name,"r")]
+file_in = input("Put in the relative directory and jsonl file name that you want to send to gemini: ")
+file_out = input("Write the name of the file you want the results written to: ")
+
+instruct = [json.loads(l) for l in open(file_in,"r")]
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     system_instruction="Do not provide any explanations for your answer choices.")
-response = model.generate_content("Write a song about puffins.")
-print(response.text)
-print(response)
+
+@limits(calls=15,period=MINUTE)    
+def get_ans(): 
+    gemini_ans = []
+    for i in instruct:
+        task = i["instruction"]
+        choices = i["instances"][0]["input"]
+        instruction = task + "\n" + choices 
+        gemini_ans.append(model.generate_content(instruction))
+    return gemini_ans
+
+ans = get_ans()
+with open(file_out, "w") as results:
+    for i in ans:
+        results.write(f"{i}\n")    
