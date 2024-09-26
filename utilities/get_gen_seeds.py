0c78162738f3de576ba3bae3c076e9c3cad4491b
mshich <mollie.shichman@gmail.com>
2024-09-26 12:04:18 -0400

    deleted data i didn't need, fixed gen script and split data in gen/leftover

--- utilities/get_gen_seeds.py
+++ utilities/get_gen_seeds.py
@@ -1,6 +1,9 @@
 import json
 import random
 
+# this script goes through the original data created in the DMR work that was subsequently multiple-choice-ified
+# it then splits the data into data used for synthetic data generation and leftovers that can be used for 1-shots
+# and evaluation. 
 NUM_SHOT = 5
 instruct = [json.loads(l) for l in open("../seed_data/seed_tasks_earthquake_mc_all.jsonl")]
 
@@ -24,9 +27,13 @@ with open("../seed_data/seed_tasks_earthquake_gen.jsonl","w") as output_gen:
                 else:
                     gen_lines = curr_lines
                     curr_lines = []
-                if len(curr_lines) != NUM_SHOT:
+                # if there's left over lines, remove selected lines from current
+                if len(curr_lines) > NUM_SHOT:
+                    print(f"Curr lines before removal: {curr_lines}")
                     for foo in gen_lines:
                         curr_lines.remove(foo)
+                    print(f"Curr lines after removal: {curr_lines}")
+                # format and write out lines used for data generation
                 new_gen_idx = 0
                 for q in gen_lines:
                     q["name"] = f"{curr_template}_{new_gen_idx}"
@@ -35,11 +42,15 @@ with open("../seed_data/seed_tasks_earthquake_gen.jsonl","w") as output_gen:
                     counter_gen += 1
                     json.dump(q, output_gen)
                     output_gen.write("\n")
+                # format and write out leftover lines
                 new_eval_idx = 0
                 for p in curr_lines:
                     p["name"] = f"{curr_template}_{new_eval_idx}"
                     new_eval_idx += 1
-                    q["id"] = f"eval_task_{counter_gen}"
+                    p["id"] = f"eval_task_{counter_eval}"
                     counter_eval += 1
-                    json.dump(q, output_eval)
+                    json.dump(p, output_eval)
                     output_eval.write("\n")
+                # update parameters for next round
+                curr_lines = [l]
+                curr_template = curr_name
