718ee3edc5ac81feee4bee31a75b26202fb8a594
mshich <mollie.shichman@gmail.com>
2024-11-25 14:58:42 -0500

    made gen script run for all cats, cleaning up data

--- gemini/gemini_ans_gen.py
+++ gemini/gemini_ans_gen.py
@@ -15,6 +15,9 @@ from rouge_score import rouge_scorer
 
 import fire
 
+CATEGORY = ["biggest","heaviest","fits","interact","can_do","can_do_size","can_do_shape","can_do_char","can_do_goal",\
+    "difference","diff_criteria", "use_as", "is_a","types_of","injury","danger","damage_to_object","explain_use",\
+        "equip_used","equip_in_task","obj_loc","objs_in_loc","secondary_use","often_use","know_use","earthquake","instruct","follow-up"]
 # batch_selfinstruct_generate.py
 
 # run:
@@ -112,94 +115,94 @@ def find_word_in_string(w, s):
 def generate_instruction_following_data(
     output_dir="../gemini_results/",
     input_dir="../seed_data/seed_tasks_earthquake_gen.jsonl",
-    category="biggest",
-    num_instructions_to_generate=100,
+    num_instructions_to_generate=40,
     mod_name="gemini-1.5-flash",
     temperature=1.1,
     num_cpus=1,
 ):
-    # get the relevent seed instructions for each category
-    instruct = [json.loads(l) for l in open(input_dir,"r")]
-    seed_instructs = [{"instruction": t["instruction"], "input": t["instances"][0]["input"], "output": t["instances"][0]["output"]} for t in instruct if t["cat"] == category]
-    print(f"Loaded {len(seed_instructs)} human-written seed instructions")
+    for cat in CATEGORY:
+        # get the relevent seed instructions for each category
+        instruct = [json.loads(l) for l in open(input_dir,"r")]
+        seed_instructs = [{"instruction": t["instruction"], "input": t["instances"][0]["input"], "output": t["instances"][0]["output"]} for t in instruct if t["cat"] == category]
+        print(f"Loaded {len(seed_instructs)} human-written seed instructions")
 
-    os.makedirs(output_dir, exist_ok=True)
-    request_idx = 0
-    # load the LM-generated instructions
-    machine_instruction_data = []
-    if os.path.exists(os.path.join(output_dir, f"{category}.json")):
-        machine_instruction_data = json.load(open(os.path.join(output_dir, f"{category}.json")))
-        print(f"Loaded {len(machine_instruction_data)} machine-generated instructions")
+        os.makedirs(output_dir, exist_ok=True)
+        request_idx = 0
+        # load the LM-generated instructions
+        machine_instruction_data = []
+        if os.path.exists(os.path.join(output_dir, f"{cat}.json")):
+            machine_instruction_data = json.load(open(os.path.join(output_dir, f"{cat}.json")))
+            print(f"Loaded {len(machine_instruction_data)} machine-generated instructions")
 
-    # similarities = {}
-    scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=False)
+        # similarities = {}
+        scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=False)
 
-    # set up gemini chit chat
-    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-    model = genai.GenerativeModel(
-        model_name=mod_name,
-        system_instruction="""You will be creating multiple choice questions on a variety of topics related to
-        common sense and/or earthquake knowledge. Be creative in choosing the vocabulary and phrasing of these questions.
-        All responses must be given as json objects with the following format: \{\"instruction\":\"example instruction\", \"input\":\"A) this\tB) is\tC) an\tD) example\t E) question\",\"output\":\"E) Question\"\}""")
-    
-    progress_bar = tqdm.tqdm(total=num_instructions_to_generate)
-    if machine_instruction_data:
-        progress_bar.update(len(machine_instruction_data))
+        # set up gemini chit chat
+        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+        model = genai.GenerativeModel(
+            model_name=mod_name,
+            system_instruction="""You will be creating multiple choice questions on a variety of topics related to
+            common sense and/or earthquake knowledge. Be creative in choosing the vocabulary and phrasing of these questions.
+            All responses must be given as json objects with the following format: \{\"instruction\":\"example instruction\", \"input\":\"A) this\tB) is\tC) an\tD) example\t E) question\",\"output\":\"E) Question\"\}""")
+        
+        progress_bar = tqdm.tqdm(total=num_instructions_to_generate)
+        if machine_instruction_data:
+            progress_bar.update(len(machine_instruction_data))
 
-    # tokenize instruction for ROUGE scoring
-    all_instructions = [d["instruction"] for d in seed_instructs] + [
-        d["instruction"] for d in machine_instruction_data
-    ]
-    all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
-    
-    while len(machine_instruction_data) < num_instructions_to_generate:
-        request_idx += 1
-        prompt_descs = [json.loads(l) for l in open("./gemini_gen_prompts.jsonl","r")]
-        prompt_desc = [p for p in prompt_descs if p["cat"] == category][0]["prompt"]
-        prompt = encode_prompt(seed_instructs, prompt_desc)
+        # tokenize instruction for ROUGE scoring
+        all_instructions = [d["instruction"] for d in seed_instructs] + [
+            d["instruction"] for d in machine_instruction_data
+        ]
+        all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
+        
+        while len(machine_instruction_data) < num_instructions_to_generate:
+            request_idx += 1
+            prompt_descs = [json.loads(l) for l in open("./gemini_gen_prompts.jsonl","r")]
+            prompt_desc = [p for p in prompt_descs if p["cat"] == cat][0]["prompt"]
+            prompt = encode_prompt(seed_instructs, prompt_desc)
 
-        request_start = time.time()
-        check_lim()
-        results = model.generate_content(prompt)
-        request_duration = time.time() - request_start
+            request_start = time.time()
+            check_lim()
+            results = model.generate_content(prompt)
+            request_duration = time.time() - request_start
 
-        process_start = time.time()
-        instruction_data = []
-        new_instructions = post_process_response(NUM_PROMPT_INSTRUCTIONS, results)
-        instruction_data += new_instructions
+            process_start = time.time()
+            instruction_data = []
+            new_instructions = post_process_response(NUM_PROMPT_INSTRUCTIONS, results)
+            instruction_data += new_instructions
 
-        total = len(instruction_data)
-        keep = 0
-        for instruction_data_entry in instruction_data:
-            # computing similarity with the pre-tokenzied instructions
-            instr_temp = instruction_data_entry["instruction"]
-            input_temp = instruction_data_entry["input"]
-            to_tokenize = f"{instr_temp} {input_temp}"
-            new_instruction_tokens = scorer._tokenizer.tokenize(to_tokenize)
-            with Pool(num_cpus) as p:
-                rouge_scores = p.map(
-                    partial(rouge_scorer._score_lcs, new_instruction_tokens),
-                    all_instruction_tokens,
-                )
-            rouge_scores = [score.fmeasure for score in rouge_scores]
-            most_similar_instructions = {
-                all_instructions[i]: rouge_scores[i] for i in np.argsort(rouge_scores)[-10:][::-1]
-            }
-            if max(rouge_scores) > 0.8:
-                continue
-            else:
-                keep += 1
-            instruction_data_entry["most_similar_instructions"] = most_similar_instructions
-            instruction_data_entry["avg_similarity_score"] = float(np.mean(rouge_scores))
-            machine_instruction_data.append(instruction_data_entry)
-            all_instructions.append(instruction_data_entry["instruction"])
-            all_instruction_tokens.append(new_instruction_tokens)
-            progress_bar.update(1)
-        process_duration = time.time() - process_start
-        print(f"Request {request_idx} took {request_duration:.2f}s, processing took {process_duration:.2f}s")
-        print(f"Generated {total} instructions, kept {keep} instructions")
-        out = open(os.path.join(output_dir, f"{category}.json"),"w")
-        json.dump(machine_instruction_data, out)
+            total = len(instruction_data)
+            keep = 0
+            for instruction_data_entry in instruction_data:
+                # computing similarity with the pre-tokenzied instructions
+                instr_temp = instruction_data_entry["instruction"]
+                input_temp = instruction_data_entry["input"]
+                to_tokenize = f"{instr_temp} {input_temp}"
+                new_instruction_tokens = scorer._tokenizer.tokenize(to_tokenize)
+                with Pool(num_cpus) as p:
+                    rouge_scores = p.map(
+                        partial(rouge_scorer._score_lcs, new_instruction_tokens),
+                        all_instruction_tokens,
+                    )
+                rouge_scores = [score.fmeasure for score in rouge_scores]
+                most_similar_instructions = {
+                    all_instructions[i]: rouge_scores[i] for i in np.argsort(rouge_scores)[-10:][::-1]
+                }
+                if max(rouge_scores) > 0.8:
+                    continue
+                else:
+                    keep += 1
+                instruction_data_entry["most_similar_instructions"] = most_similar_instructions
+                instruction_data_entry["avg_similarity_score"] = float(np.mean(rouge_scores))
+                machine_instruction_data.append(instruction_data_entry)
+                all_instructions.append(instruction_data_entry["instruction"])
+                all_instruction_tokens.append(new_instruction_tokens)
+                progress_bar.update(1)
+            process_duration = time.time() - process_start
+            print(f"Request {request_idx} took {request_duration:.2f}s, processing took {process_duration:.2f}s")
+            print(f"Generated {total} instructions, kept {keep} instructions")
+            out = open(os.path.join(output_dir, f"{cat}.json"),"w")
+            json.dump(machine_instruction_data, out)
         
 
 
