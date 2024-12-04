29173d3d9f750f74f24840cea507b79aa2f080cb
mshich <mollie.shichman@gmail.com>
2024-12-04 17:28:31 -0500

    eval now formatted properly

--- utilities/clean_up_seeds.py
+++ utilities/clean_up_seeds.py
@@ -1,7 +1,7 @@
 import json
 
-inputs = [json.loads(l) for l in open("../seed_data/seed_tasks_earthquake_gen2.jsonl")]
-with open("../seed_data/seed_tasks_earthquake_gen.jsonl", "w") as outer:
+inputs = [json.loads(l) for l in open("../seed_data/seed_tasks_eval2.jsonl")]
+with open("../seed_data/seed_tasks_eval.jsonl", "w") as outer:
     counter = 0
     new_vals = []
     for i in inputs:
@@ -9,7 +9,7 @@ with open("../seed_data/seed_tasks_earthquake_gen.jsonl", "w") as outer:
         temp_name_arr.pop()
         curr_name = "_".join(temp_name_arr)
         i["name"] = curr_name
-        i["id"] = f"gen_task_{counter}"
+        i["id"] = f"eval_task_{counter}"
         new_vals.append(i)
         counter += 1
     for v in new_vals:
