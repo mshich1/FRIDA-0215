96b8b2b948d3a5d5038dd9fa753c28b822943508
mshich <mollie.shichman@gmail.com>
2024-12-04 17:47:41 -0500

    got rid of it, making peace with the error

--- llama/get_ft_ans.py
+++ llama/get_ft_ans.py
@@ -11,7 +11,6 @@ adapter_model_path = "../../"
 adapter_model_names = ["rel_size","can_do_it","is_a_dif","risky","equip","obj_facts","quake","instr","all"]
 
 tokenized = AutoTokenizer.from_pretrained(base_model_name)
-tokenized.pad_token = tokenized.eos_token
 
 eval_qs = [json.loads(l) for l in open("../seed_data/seed_tasks_eval.jsonl")]
 for a in adapter_model_names:
@@ -23,10 +22,8 @@ for a in adapter_model_names:
         for ev in tqdm(eval_qs):
             query = [{"role":"system", "content": "The following is a multiple choice question about object properties and earthquakes. There is only one correct answer. Your answer should repeat the correct answer exactly with no explanation."},{"role": "user", "content": f"{ev['instruction']} {ev['instances'][0]['input']}"}]
             chat = tokenized.apply_chat_template(query, tokenize=False, add_generation_prompt=True)
-            tokenized_chat = tokenized.encode(chat, return_tensors="pt", padding=True)
-            attention_mask = tokenized_chat["attention_mask"]
-            tokenized_chat = tokenized_chat.to("cuda")
-            output = model.generate(tokenized_chat, max_new_tokens=128,  attention_mask=attention_mask, pad_token_id=tokenized.eos_token_id)
+            tokenized_chat = tokenized.encode(chat, return_tensors="pt", padding=True).to("cuda")
+            output = model.generate(tokenized_chat, max_new_tokens=128, pad_token_id=tokenized.eos_token_id)
             ans = tokenized.decode(output[0])
             ans_matched = re.search("<\|start_header_id\|>assistant<\|end_header_id\|>\s*(.*)<\|eot_id\|>", ans)
             ans_spot.write(ans_matched.group(1))
