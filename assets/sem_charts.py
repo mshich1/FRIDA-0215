b6e6856293c7ddac1691540abe8ed1e9f30bb220
mshich <mollie.shichman@gmail.com>
2024-12-09 12:15:55 -0500

    made semscore charts

--- assets/sem_charts.py
+++ assets/sem_charts.py
@@ -2,24 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
-with open("../llama_results/em.txt") as l_in, open("../gemini_results/base_em.txt") as b_in:
+with open("../llama_results/sem.txt") as l_in, open("../gemini_results/base_sem.txt") as b_in:
     scores_l = [l for l in l_in]
     scores_b = [b for b in b_in]
     scores = scores_l + scores_b
     ems = {"overall":[], "rel_size":[],"can_do_it": [], "is_a_dif":[],"risky":[],\
            "equip":[], "obj_facts":[], "quake":[], "instr": []}
     for p in scores:
-        cat_nums = re.match("([a-zA-Z_]+) accuracy: {\'exact_match\': (0\.\d+|1.0)}", p)
+        cat_nums = re.match("([a-zA-Z_]+) average sem score: (0\.\d+|1.0)", p)
         if cat_nums is None:
             continue
         cat = cat_nums.group(1)
         num = float(cat_nums.group(2))
         ems[cat].append(num)
-    
-    del ems["overall"]
-    del ems["rel_size"]
-    del ems["can_do_it"]
-    del ems["is_a_dif"]
+
     x_axis_labels = ["size\nmodel","can do\nmodel","dif and\nhypernym model","object risk\nmodel",\
                      "equipment\nmodel","object facts\nmodel","quake\nmodel","instruction id\nmodel",\
                      "all synth\ndata model", "gemini", "llama3.1\ninstruct"]
@@ -31,10 +27,10 @@ with open("../llama_results/em.txt") as l_in, open("../gemini_results/base_em.tx
         print(v)
         print(len(v))
         width = 0.25
-        plt.bar(x, winning_scores, width, color='cornflowerblue')
+        plt.bar(x, winning_scores, width, color='coral')
         plt.xticks(x, x_axis_labels)
         plt.xlabel("Model")
-        plt.ylabel("Exact Match")
+        plt.ylabel("Cosine similarity of model answer embedding vs gold standard embedding")
         plt.ylim(0,1)
-        plt.title(f"LM's Exact Match score for {k} data")
+        plt.title(f"LM embedding similarity score for {k} data")
         plt.show()
