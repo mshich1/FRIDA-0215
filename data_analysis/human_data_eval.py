802629250445d571d230b2cdc8304e9b9e4e5fdb
mshich <mollie.shichman@gmail.com>
2025-02-14 14:58:48 -0500

    got stats for annotator agreement and avg scores

--- data_analysis/human_data_eval.py
+++ data_analysis/human_data_eval.py
@@ -1,7 +1,7 @@
 # This script is to get the average reasonableness and informativeness scores
 # from disagree.agreements import BiDisagreements
 import pandas as pd
-from disagree import Metrics, Krippendorff
+from metrics import Metrics, Krippendorff
 
 inf = {'a':[],'c':[],'t':[]}
 reas = {'a':[],'c':[],'t':[]}
@@ -44,24 +44,38 @@ with open("human_stats.txt","w") as outie:
     rk = reas_krip.alpha(data_type="nominal")
     outie.write(f"Krippendorf Alpha reason:{rk}\n")
     # outie.write(f"naive kappa reason: {calc_reas.kappa()}")
-    temp = reas_data.kappa('a','c')
-    outie.write(f"Average Agreement a c reason: {temp}")
+    ack = reas_data.cohens_kappa('a','c')
+    atk = reas_data.cohens_kappa('a','t')
+    ctk = reas_data.cohens_kappa('t','c')
+    outie.write(f"cohen's kappa agreement a c reason: {ack}\n")
+    outie.write(f"cohen's kappa agreement a t reason: {atk}\n")
+    outie.write(f"cohen's kappa agreement c t reason: {ctk}\n")
+    outie.write(f"Fleiss Kappa reason: {reas_data.fleiss_kappa()}\n\n")
+
     ik = inf_krip.alpha(data_type="nominal")
     outie.write(f"Krippendorf Alpha inform:{ik}\n")
+    ack = inf_data.cohens_kappa('a','c')
+    atk = inf_data.cohens_kappa('a','t')
+    ctk = inf_data.cohens_kappa('t','c')  
+    outie.write(f"cohen's kappa agreement a c inform: {ack}\n")
+    outie.write(f"cohen's kappa agreement a t inform: {atk}\n")
+    outie.write(f"cohen's kappa agreement c t inform: {ctk}\n") 
+    outie.write(f"Fleiss Kappa inform: {inf_data.fleiss_kappa()}\n\n") 
+
     # outie.write(f"naive kappa inform: {calc_inf.kappa()}")
     # outie.write(f"Average Observed Agreement inform: {calc_inf.avg_Ao()}")
     def write_averages(list, type):
         # c_list = [i for i in list if i[0]=='c']
         # for i in range(len(c_list)):
         #     print(f"t[2] at index {i}: {c_list[i][2]}")
-        t_vals = [int(i[1]) for i in list if i[0] == "t"]
-        c_vals = [int(i[1]) for i in list if i[0] == "c"]
-        a_vals = [int(i[1]) for i in list if i[0] == "a"]
-        all_vals = [int(i[1]) for i in list]
-        outie.write(f"Average {type} value overall: {sum(all_vals)/len(all_vals)}\n")
-        outie.write(f"Average {type} value Taylor: {sum(t_vals)/len(t_vals)}\n")
-        outie.write(f"Average {type} value Claire: {sum(c_vals)/len(c_vals)}\n")
-        outie.write(f"Average {type} value Austin: {sum(a_vals)/len(a_vals)}\n\n")
+        int_a = [int(i) for i in list['a']]
+        int_c = [int(i) for i in list['c']]
+        int_t = [int(i) for i in list['t']]
+        int_all = int_a + int_c + int_t
+        outie.write(f"Average {type} value overall: {sum(int_all)/len(int_all)}\n")
+        outie.write(f"Average {type} value Taylor: {sum(int_t)/len(int_t)}\n")
+        outie.write(f"Average {type} value Claire: {sum(int_c)/len(int_c)}\n")
+        outie.write(f"Average {type} value Austin: {sum(int_a)/len(int_a)}\n\n")
     write_averages(inf, "inform")
     write_averages(reas, "reason")
 
